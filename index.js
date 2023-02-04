ac68576eeb9fcf4219f177852a0ab06750de5d58
Rohan Jadhav <rohanjadhav@Rohans-MacBook-Air.local>
2023-02-04 15:04:48 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express"
 import cors from "cors"
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
+import bcrypt from "bcrypt";
+import multer from "multer";
+// const asyncHandler = require("express-async-handler");
+
+import formidable from "formidable"
+import _ from "lodash"
+import fs from "fs"
 
 const app = express()
 app.use(express.json())
@@ -20,14 +27,90 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
+const potholeScheme = new mongoose.Schema({
+    email: String,
+    address: String,
+    pincode: Number,
+    city: String,
+    state: String,
+    potholeImage: String
+})
+
 const User = new mongoose.model("User", userSchema)
+const potholes = new mongoose.model("DetailsPot", potholeScheme)
+
+//Functions
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "../frontend/public/uploads")
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+})
+
+const upload = multer({storage: storage})
 
 //Routes
+app.post("/add",upload.single("potholeImage"), (req, res) => {
+    // const newPot = new potholes({
+    //     email: req.body.email,
+    //     address: req.body.address,
+    //     pincode: req.body.pincode,
+    //     city: req.body.city,
+    //     state: req.body.state,
+    //     potholeImage: req.file.potholeImage
+    // })
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "problem with image",
+      });
+    }
+    //destructure the fields
+    const { email, address, pincode,city,state } = fields;
+
+    if (!email || !address || !pincode || !city || !state) {
+      return res.status(400).json({
+        error: "Please include all fields",
+      });
+    }
+
+    let pothole = new potholes(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size too big!",
+        });
+      }
+      pothole.photo.data = fs.readFileSync(file.photo.path);
+      pothole.photo.contentType = file.photo.type;
+    }
+    // console.log(product);
+
+    //save to the DB
+    product.save((err, pothole) => {
+      if (err) {
+        res.status(400).json({
+          error: "Saving tshirt in DB failed",
+        });
+      }
+      res.json(pothole);
+    });
+  });
+})
+
 app.post("/login", (req, res)=> {
     const { email, password} = req.body
     User.findOne({ email: email}, (err, user) => {
         if(user){
-            if(password === user.password ) {
+            if((bcrypt.compare(password, user.password))) {
                 res.send({message: "Login Successfull", user: user})
             } else {
                 res.send({ message: "Password didn't match"})
@@ -44,10 +127,13 @@ app.post("/register", (req, res)=> {
         if(user){
             res.send({message: "User already registerd"})
         } else {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+
             const user = new User({
                 name,
                 email,
-                password
+                password: hash
             })
             user.save(err => {
                 if(err) {
